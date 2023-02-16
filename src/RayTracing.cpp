24c7c25c29d6a615395efbfb90d2b7dc95ba46c2
invi1998 <2267661826@qq.com>
2023-02-16 17:51:32 +0800

    玻璃材质

--- src/RayTracing.cpp
+++ src/RayTracing.cpp
@@ -48,13 +48,13 @@ int main()
     hittable_list world;
 
     auto material_ground = std::make_shared<lambertian>(color(0.8, 0.6, 0.1));
-    auto material_center = std::make_shared<dielectric>(1.5);
-    auto material_left = std::make_shared<dielectric>(1.5);
+    auto material_center = std::make_shared<dielectric>(1.7);
+    auto material_left = std::make_shared<lambertian>(color(0.2, 0.5, 0.8));
     auto material_right = std::make_shared<metal>(color(0.6, 0.6, 0.8), 1.0);
 
     world.add(std::make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(std::make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
-    world.add(std::make_shared<sphere>(point3(-1.0, 0.5, -1.0), 0.5, material_left));
+    world.add(std::make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
     world.add(std::make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
     // Camera
