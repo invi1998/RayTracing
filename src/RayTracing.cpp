242037cae4066e16781d5d16a55f5e4b1de35d18
invi1998 <2267661826@qq.com>
2023-02-16 19:40:37 +0800

    最终结果，一个随机场景渲染

--- src/RayTracing.cpp
+++ src/RayTracing.cpp
@@ -35,36 +35,84 @@ color ray_color(const ray &r, const hittable &world, int depth)
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.6, 0.5, 1.0);
 }
 
+hittable_list random_scene()
+{
+    hittable_list world;
+
+    auto ground_material = std::make_shared<lambertian>(color(0.5, 0.4, 0.4));
+
+    world.add(std::make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
+
+    for (int a = -15; a < 15; a++)
+    {
+        for (int b = -15; b < 15; b++)
+        {
+            auto choose_mat = random_double();
+            point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
+
+            if ((center - point3(4, 0.2, 0)).length() > 0.8)
+            {
+                std::shared_ptr<material> sphere_material;
+
+                if (choose_mat < 0.8)
+                {
+                    // 漫反射材质
+                    auto albedo = color::random() * color::random();
+                    auto size = random_double(0.1, 0.3);
+                    sphere_material = std::make_shared<lambertian>(albedo);
+                    world.add(std::make_shared<sphere>(center, size, sphere_material));
+                }
+                else if (choose_mat < 0.95)
+                {
+                    // 金属材质
+                    auto size = random_double(0.1, 0.3);
+                    auto albedo = color::random(0.5, 1);
+                    auto fuzz = random_double(0, 0.5);
+                    sphere_material = std::make_shared<metal>(albedo, fuzz);
+                    world.add(std::make_shared<sphere>(center, size, sphere_material));
+                }
+                else
+                {
+                    // 玻璃材质
+                    auto fu = random_double(1, 2);
+                    auto size = random_double(0.1, 0.3);
+                    sphere_material = std::make_shared<dielectric>(fu);
+                    world.add(std::make_shared<sphere>(center, size, sphere_material));
+                }
+            }
+        }
+    }
+    auto material1 = std::make_shared<dielectric>(1.5);
+    world.add(std::make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+
+    auto material2 = std::make_shared<lambertian>(color(0.4, 0.2, 0.1));
+    world.add(std::make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+
+    auto material3 = std::make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+    world.add(std::make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+
+    return world;
+}
+
 int main()
 {
     // Image
-    const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 600;
+    const auto aspect_ratio = 3.0 / 2.0;
+    const int image_width = 1200;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 100; // 采样率
+    const int samples_per_pixel = 500; // 采样率
     const int max_depth = 50;
 
     // world
-    hittable_list world;
-
-    auto material_ground = std::make_shared<lambertian>(color(0.8, 0.6, 0.1));
-    auto material_center = std::make_shared<dielectric>(1.7);
-    auto material_left = std::make_shared<lambertian>(color(0.2, 0.5, 0.8));
-    auto material_right = std::make_shared<metal>(color(0.6, 0.6, 0.8), 1.0);
-
-    world.add(std::make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
-    world.add(std::make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
-    world.add(std::make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
-    world.add(std::make_shared<sphere>(point3(0.3, 0.0, 0.5), 0.1, material_center));
-    world.add(std::make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
-
-    point3 lookfrom(3, 3, 2);
-    point3 lookat(0, 0, -1);
-    vec3 vup(0, 1, 0);
-    auto dist_to_focus = (lookfrom - lookat).length();
-    auto aperture = 2.0;
+    hittable_list world = random_scene();
 
     // Camera
+    point3 lookfrom(13, 2, 3);
+    point3 lookat(0, 0, 0);
+    vec3 vup(0, 1, 0);
+    auto dist_to_focus = 10.0;
+    auto aperture = 0.1;
+
     camera cam(point3(-2, 2, 1), point3(0, 0, -1), vec3(0, 1, 0), 20.0, aspect_ratio, aperture, dist_to_focus);
 
     // Render
