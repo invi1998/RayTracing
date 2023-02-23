fda97c78f97932e4d82f6e5085f81721b768e85e
invi1998 <2267661826@qq.com>
2023-02-23 17:15:34 +0800

    大理石材质纹理渲染

--- src/include/perlin.h
+++ src/include/perlin.h
@@ -8,6 +8,7 @@ class perlin
 {
 private:
     static const int point_count = 256;
+    vec3 *ranvec;
     double *ranfloat;
     int *perm_x;
     int *perm_y;
@@ -55,21 +56,47 @@ private:
         return accum;
     }
 
+    static double perlin_interp(vec3 c[2][2][2], double u, double v, double w)
+    {
+        auto uu = u * u * (3 - 2 * u);
+        auto vv = v * v * (3 - 2 * v);
+        auto ww = w * w * (3 - 2 * w);
+
+        auto accm = 0.0;
+
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                for (int k = 0; k < 2; k++)
+                {
+                    vec3 weight_v(u - i, v - j, w - k);
+                    accm += (i * uu + (1 - i) * (1 - uu)) *
+                            (j * vv + (1 - j) * (1 - vv)) *
+                            (k * ww + (1 - k) * (1 - ww)) * dot(c[i][j][k], weight_v);
+                }
+            }
+        }
+        return accm;
+    }
+
 public:
     perlin()
     {
-        ranfloat = new double[point_count];
+        ranvec = new vec3[point_count];
         for (int i = 0; i < point_count; ++i)
         {
-            ranfloat[i] = random_double();
+            ranvec[i] = unit_vector(vec3::random(-1, 1));
         }
 
         perm_x = perlin_generate_perm();
         perm_y = perlin_generate_perm();
         perm_z = perlin_generate_perm();
     }
+
     ~perlin()
     {
+        delete[] ranvec;
         delete[] ranfloat;
         delete[] perm_x;
         delete[] perm_y;
@@ -90,7 +117,7 @@ public:
         auto j = static_cast<int>(floor(p.y()));
         auto k = static_cast<int>(floor(p.z()));
 
-        double c[2][2][2];
+        vec3 c[2][2][2];
 
         for (int di = 0; di < 2; di++)
         {
@@ -98,14 +125,30 @@ public:
             {
                 for (int dk = 0; dk < 2; dk++)
                 {
-                    c[di][dj][dk] = ranfloat[perm_x[(i + di) & 255] ^
-                                             perm_y[(j + dj) & 255] ^
-                                             perm_z[(k + dk) & 255]];
+                    c[di][dj][dk] = ranvec[perm_x[(i + di) & 255] ^
+                                           perm_y[(j + dj) & 255] ^
+                                           perm_z[(k + dk) & 255]];
                 }
             }
         }
 
-        return trilinear_interp(c, u, v, w);
+        return perlin_interp(c, u, v, w);
+    }
+
+    double turb(const point3 &p, int depth = 7) const
+    {
+        auto accum = 0.0;
+        auto temp_p = p;
+        auto weight = 1.0;
+
+        for (int i = 0; i < depth; i++)
+        {
+            accum += weight * noise(temp_p);
+            weight *= 0.5;
+            temp_p *= 2;
+        }
+
+        return abs(accum);
     }
 };
 
